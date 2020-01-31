a7fb8e854b13212a070918ad0404e997e781929c
Rintu Gochhayat <rintuweb4@gmail.com>
2020-01-31 17:47:09 +0530

    Updates

---
+++ precache-manifest.0e33ed3a8d2b41dd2ca01411086bc637.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2380dcbae2c70bf16f535426cde5ec44",
+    "url": "/React-Web-App/index.html"
+  },
+  {
+    "revision": "120f89d4bbb8bd9089a4",
+    "url": "/React-Web-App/static/css/2.ee8f07d3.chunk.css"
+  },
+  {
+    "revision": "6f1821bfd73483c51067",
+    "url": "/React-Web-App/static/css/main.969e681a.chunk.css"
+  },
+  {
+    "revision": "120f89d4bbb8bd9089a4",
+    "url": "/React-Web-App/static/js/2.8d4f6992.chunk.js"
+  },
+  {
+    "revision": "57298b49bd6ed2c03c2216d2b1a7ee91",
+    "url": "/React-Web-App/static/js/2.8d4f6992.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f1821bfd73483c51067",
+    "url": "/React-Web-App/static/js/main.8a364dbf.chunk.js"
+  },
+  {
+    "revision": "0cee8eef2b012438df91",
+    "url": "/React-Web-App/static/js/runtime-main.5554b659.js"
+  },
+  {
+    "revision": "448b8a617f3d0b2a064255e338280d77",
+    "url": "/React-Web-App/static/media/1.448b8a61.jpg"
+  },
+  {
+    "revision": "fabde769b1130d7798a6cf343a66f1ad",
+    "url": "/React-Web-App/static/media/2.fabde769.jpg"
+  },
+  {
+    "revision": "09cd79b5059cc31b16e91e24ba718d72",
+    "url": "/React-Web-App/static/media/3.09cd79b5.jpg"
+  },
+  {
+    "revision": "6fce2a4bf2746442157598ae33f25e7c",
+    "url": "/React-Web-App/static/media/banner-bg.6fce2a4b.png"
+  },
+  {
+    "revision": "3edcb0c6a5635d6784f0c1fec4315009",
+    "url": "/React-Web-App/static/media/footer-bg.3edcb0c6.png"
+  },
+  {
+    "revision": "b4d28ea9d1d6a511879f385f9475e614",
+    "url": "/React-Web-App/static/media/left-image.b4d28ea9.png"
+  },
+  {
+    "revision": "fa18c8159977039522431d3c8ced2748",
+    "url": "/React-Web-App/static/media/picT1.fa18c815.png"
+  },
+  {
+    "revision": "3f15c2f43bd564a64c373d387d8ebd72",
+    "url": "/React-Web-App/static/media/picT2.3f15c2f4.png"
+  },
+  {
+    "revision": "66779ecc42a5b7668085bfe60ea9ab9c",
+    "url": "/React-Web-App/static/media/picT3.66779ecc.png"
+  },
+  {
+    "revision": "ad3b1fed2eb124095c6d8f3e3508de45",
+    "url": "/React-Web-App/static/media/statistic.ad3b1fed.jpg"
+  }
+]);
